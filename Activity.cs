2c16411c13a83c3a2a42304c009c55bf424b8cbf
John Schneider <John.M.Schneider@outlook.com>
2022-09-30 18:35:20 -0500

    Add project files.

---
+++ Activity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameAuto {
+    public interface Activity {
+        /// <summary>
+        /// Determines if this activity is currently active and should receive a call to <see cref="Run"/>.
+        /// </summary>
+        public bool IsActive();
+
+        /// <summary>
+        /// Called when this Activity activates due to <see cref="IsActive"/>
+        /// </summary>
+        /// <returns>Return true to prevent further Activities from executing.</returns>
+        public bool Run();
+
+    }
+}
---
+++ ActivityMonitor.cs
@@ -0,0 +1,19 @@
+﻿namespace GameAuto {
+    public class ActivityMonitor {
+
+        private List<Activity> activities = new List<Activity>();
+
+        public void AddActivity( Activity activity ) {
+            activities.Add( activity );
+        }
+
+        public void RemoveActivity( Activity activity ) {
+            activities.Remove( activity );
+        }
+
+        public void Start() {
+
+        }
+
+    }
+}
